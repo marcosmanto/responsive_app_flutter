e75f0650980ee76317b61696904e8085e63a285f
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-11-21 21:08:05 -0300

    Enabled dragging scrolling feature for web project (when device uses mouse). This is because the ScrollBehavior.dragDevices by default allows scrolling widgets to be dragged by all PointerDeviceKind except for PointerDeviceKind.mouse

--- lib/responsive_app_flutter.dart
+++ lib/responsive_app_flutter.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:responsive_app_flutter/pages/home_page.dart';
@@ -13,8 +15,17 @@ class PlacesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Places App',
+      scrollBehavior: MyCustomScrollBehavior(),
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: const HomePage(),
     );
   }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
